02941e76f3bab97714e518af8a183dd700e8b14f
Eliezer Nsengi <avaranstudios@gmail.com>
2024-05-17 09:27:53 +0200

    gh-updte: configuring tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#333333",
+        secondary: "#D5D5D5",
+        brownish: "#A18A4D",
+        black: "#333333",
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   plugins: [],
-}
-
+};
